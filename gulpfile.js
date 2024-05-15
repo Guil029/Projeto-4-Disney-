561f0004c2b6b5ce587f14b5567257ccf23a0224
guigo <149022375+Guil029@users.noreply.github.com>
2024-05-15 02:44:54 -0300

    images added

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-    return gulp.src('./src/*.scss')
+    return gulp.src('./src/styles/*.scss')
     .pipe(sass({ outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
+
 exports.watch = function() {
     gulp.watch('./src/*.scss', gulp.parallel(styles))
 }
